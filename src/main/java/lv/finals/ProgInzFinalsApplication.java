bee62dece4040caf984f1953821d7ccb072ce3e1
linaXta <alina.rubcova0418@gmail.com>
2023-05-19 10:50:58 +0200

    New project

---
+++ src/main/java/lv/finals/ProgInzFinalsApplication.java
@@ -0,0 +1,13 @@
+package lv.finals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgInzFinalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgInzFinalsApplication.class, args);
+	}
+
+}
